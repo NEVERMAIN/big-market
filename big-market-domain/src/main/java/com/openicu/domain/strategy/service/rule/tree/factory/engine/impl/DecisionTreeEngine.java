f96a114791634e6bdfbffd7d5ec05da18bacfe8d
yunqiji <xiaoxiongchenCXXcc@outlook.com>
2024-08-10 23:51:12 +0800

    fix:1. 默认抽取到积分奖品。     2. 抽取到其他奖品，但无库存时，走积分兜底奖品。     3. 配置的黑名单用户，在 0.01 ~ 1 积分的随机奖品。

--- big-market-domain/src/main/java/com/openicu/domain/strategy/service/rule/tree/factory/engine/impl/DecisionTreeEngine.java
+++ big-market-domain/src/main/java/com/openicu/domain/strategy/service/rule/tree/factory/engine/impl/DecisionTreeEngine.java
@@ -66,31 +66,61 @@ public class DecisionTreeEngine implements IDecisionTreeEngine {
         return strategyAwardData;
     }
 
+    /**
+     * 根据给定的问题值和规则树节点列表，确定下一个节点
+     *
+     * @param matterValue 问题值，用于判断下一个节点的依据
+     * @param treeNodeLineVOList 规则树节点列表，包含多个规则节点信息
+     * @return 返回下一个节点的标识，如果没有找到合适的节点，返回null
+     */
     public String nextNode(String matterValue, List<RuleTreeNodeLineVO> treeNodeLineVOList) {
         // 如果是叶子节点就直接退出
         if (null == treeNodeLineVOList || treeNodeLineVOList.isEmpty()) return null;
+        // 遍历规则树节点列表，寻找匹配的节点
         for (RuleTreeNodeLineVO nodeLine : treeNodeLineVOList) {
+            // 如果问题值符合当前节点的判断逻辑
             if (decisionLogic(matterValue, nodeLine)) {
+                // 返回匹配节点的目标节点标识
                 return nodeLine.getRuleNodeTo();
             }
         }
+       // 如果没有匹配的节点，返回null
        return null;
     }
 
+
+    /**
+     * 根据规则限制类型判断某个值是否满足条件
+     *
+     * 本函数通过比较传入的值（matterValue）和规则节点线中的限制值（nodeLine.getRuleLimitValue().getCode()），
+     * 根据不同的限制类型（nodeLine.getRuleLimitType()）来判断是否满足条件
+     *
+     * @param matterValue 需要判断的值
+     * @param nodeLine 规则节点线对象，包含规则限制类型和限制值
+     * @return boolean 返回判断结果，如果满足条件返回true，否则返回false
+     */
     public boolean decisionLogic(String matterValue, RuleTreeNodeLineVO nodeLine) {
+        // 根据规则限制类型执行不同的比较逻辑
         switch (nodeLine.getRuleLimitType()) {
             case EQUAL:
+                // 判断值是否等于规则限制值
                 return matterValue.equals(nodeLine.getRuleLimitValue().getCode());
             case GT:
+                // 判断值是否大于规则限制值
                 return matterValue.compareTo(nodeLine.getRuleLimitValue().getCode()) > 0;
             case LT:
+                // 判断值是否小于规则限制值
                 return matterValue.compareTo(nodeLine.getRuleLimitValue().getCode()) < 0;
             case GE:
+                // 判断值是否大于等于规则限制值
                 return matterValue.compareTo(nodeLine.getRuleLimitValue().getCode()) >= 0;
             case LE:
+                // 判断值是否小于等于规则限制值
                 return matterValue.compareTo(nodeLine.getRuleLimitValue().getCode()) <= 0;
             default:
+                // 默认情况下返回false，表示不满足任何条件
                 return false;
         }
     }
+
 }
