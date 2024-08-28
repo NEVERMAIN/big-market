18caedbfd6ba3f230b1915b48815fb65e058acfb
yunqiji <xiaoxiongchenCXXcc@outlook.com>
2024-08-28 19:14:32 +0800

    doc:增加 Zookeeper 的单元测试

--- big-market-app/src/main/java/com/openicu/config/DCCValueBeanFactory.java
+++ big-market-app/src/main/java/com/openicu/config/DCCValueBeanFactory.java
@@ -4,6 +4,7 @@ import com.openicu.types.annotation.DCCValue;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.recipes.cache.ChildData;
 import org.apache.curator.framework.recipes.cache.CuratorCache;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
@@ -41,23 +42,15 @@ public class DCCValueBeanFactory implements BeanPostProcessor {
             log.info("DCC 节点监听 base node {} not absent create new done!", BASE_CONFIG_PATH_CONFIG);
         }
 
+        // 构建 Zookeeper 缓存
         CuratorCache curatorCache = CuratorCache.build(client, BASE_CONFIG_PATH_CONFIG);
         curatorCache.start();
 
+        // 添加 ZooKeeper 缓存监听器，监听节点变化
         curatorCache.listenable().addListener((type, oldData, data) -> {
             switch (type) {
                 case NODE_CHANGED:
-                    String dccValuePath = data.getPath();
-                    Object objBean = dccObjGroup.get(dccValuePath);
-                    if (null == objBean) return;
-                    try {
-                        Field field = objBean.getClass().getDeclaredField(dccValuePath.substring(dccValuePath.lastIndexOf("/") + 1));
-                        field.setAccessible(true);
-                        field.set(objBean, new String(data.getData()));
-                        field.setAccessible(false);
-                    } catch (Exception e) {
-                        throw new RuntimeException(e);
-                    }
+                    handleNodeChanged(data);
                     break;
                 default:
                     break;
@@ -66,6 +59,31 @@ public class DCCValueBeanFactory implements BeanPostProcessor {
 
     }
 
+    /**
+     * 处理 Zookeeper 节点值发生变化的方法
+     * @param data
+     */
+    private void handleNodeChanged(ChildData data) {
+        String dccValuePath = data.getPath();
+        Object objBean = dccObjGroup.get(dccValuePath);
+        if (null == objBean) return;
+
+        try {
+            String fieldName = dccValuePath.substring(dccValuePath.lastIndexOf("/") + 1);
+            Field field = objBean.getClass().getDeclaredField(fieldName);
+            setField(objBean, field, new String(data.getData()));
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to update field: " + dccValuePath, e);
+        }
+    }
+
+    /**
+     * 将配置的开关字段注册到 zookeeper 配置中心
+     * @param bean 对象
+     * @param beanName 对象名称
+     * @return
+     * @throws BeansException
+     */
     @Override
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
 
@@ -93,18 +111,15 @@ public class DCCValueBeanFactory implements BeanPostProcessor {
                 if (null == client.checkExists().forPath(keyPath)) {
                     client.create().creatingParentsIfNeeded().forPath(keyPath);
                     if (StringUtils.isNotBlank(defaultValue)) {
-                        field.setAccessible(true);
-                        field.set(bean, defaultValue);
-                        field.setAccessible(false);
+                        setField(bean, field, defaultValue);
                     }
                     log.info("DCC 节点监听 创建节点 {}", keyPath);
+
                 } else {
 
                     String configValue = new String(client.getData().forPath(keyPath));
                     if (StringUtils.isNotBlank(configValue)) {
-                        field.setAccessible(true);
-                        field.set(bean, configValue);
-                        field.setAccessible(false);
+                        setField(bean, field, configValue);
                         log.info("DCC 节点监听 设置配置 {} {} {}", keyPath, field.getName(), configValue);
                     }
                 }
@@ -119,5 +134,18 @@ public class DCCValueBeanFactory implements BeanPostProcessor {
 
         return bean;
 
+    }
+
+    /**
+     * 设置对象的字段值
+     * @param bean 对象
+     * @param field 字段
+     * @param configValue 字段值
+     * @throws IllegalAccessException
+     */
+    private static void setField(Object bean, Field field, String configValue) throws IllegalAccessException {
+        field.setAccessible(true);
+        field.set(bean, configValue);
+        field.setAccessible(false);
     }
 }
